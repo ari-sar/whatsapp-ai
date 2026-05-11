c080758c56e13c7a1be03e9d36793dcf9399d7d4
arindam-sarkar <arisarkar.mail@gmail.com>
2026-05-11 12:48:20 +0530

    Auto-save: 50 file(s) updated [2026-05-11 12:48]

---
+++ frontend/meetwave-client-portal/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'meetwave-client-portal',
+  webDir: 'dist'
+};
+
+export default config;
